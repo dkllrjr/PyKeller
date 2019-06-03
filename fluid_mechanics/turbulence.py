7a7243c3f5b3505656a705ed39d5d6b0e377262e
dkllrjr <dg.kllr.jr@gmail.com>
2019-06-03 09:46:28 -0800

    new file:   fluid_mechanics/__pycache__/turbulence.cpython-37.pyc 	modified:   fluid_mechanics/turbulence.py

--- fluid_mechanics/turbulence.py
+++ fluid_mechanics/turbulence.py
@@ -1,178 +1,133 @@
 #Created: Wed May 29 20:46:29 2019
 #By: mach
 
-function [turb,wind]=turb_calc(sonic,periodo)
-%% Input here is the structure containing information from the sonic anemometer
-%% ---outputs are turb components and mean wind parameters
-turb=[];
-wind=[];
-%-----calculate uprime, vprime, wprime, thetaprime and the wind speed and
-%wind direction
-%INPUT: jd: julian day in UTC and sonic:structure containing the sonic
-%anemometers data
-jd=sonic.jd;
-u=sonic.u;
-v=sonic.v;
-w=sonic.w;
-T=sonic.ts;
-%
-
-
+import numpy as np
 
 class Velocity:
     
-    def __init__(self,u,v,w,time):
-        self.time = time
-        self.u = u
-        self.v = v
-        self.w = w
+    def __init__(self):
+        self.raw = []
+        self.turbulence = []
+        self.wind = []
+    
+    def add_raw(self,u,v,w,T,t):
         
+        class Raw:
+            
+            def __init__(self,u,v,w,T,t):
+                self.u = u
+                self.v = v
+                self.w = w
+                self.T = T
+                self.t = t
+                
+        raw = Raw(u,v,w,T,t)
+        self.raw = raw
         
-        
+    def add_turbulence(self,turbulence):
+        self.turbulence.append(turbulence)
+    
+    def add_wind(self,wind):
+        self.wind.append(wind)
         
 class Turbulence:
     
-    def __init__(self,u,v,w,t,period):
-        u_mean, u_prime, t_mean, t_prime = reynolds_decomposition(u,t,period)
-        v_mean, v_prime, _, _ = reynolds_decomposition(u,t,period)
-        w_mean, w_prime, _, _ = reynolds_decomposition(u,t,period)
+    def __init__(self,u,v,w,T,t,period):
         self.period = period
-        self.u_mean = u_mean
-        self.u_prime = u_prime
-        self.v_mean = v_mean
-        self.v_prime = v_prime
-        self.w_mean = w_mean
-        self.w_prime = w_prime     
+        self.u_mean, self.u_prime = run_reynolds_decomposition(u)
+        self.v_mean, self.v_prime = run_reynolds_decomposition(v)
+        self.w_mean, self.w_prime = run_reynolds_decomposition(w)
+        self.T_mean, self.T_prime = run_reynolds_decomposition(T)
+        self.t_mean, self.t_prime = run_reynolds_decomposition(t)
+
+class Wind:
     
-def reynolds_decomposition(x,t,period):
+    def __init__(self,wind,theta,phi,period):
+        self.period = period
+        self.speed = wind
+        self.direction = theta
+        self.inclination = phi
+
+def get_velocity(u,v,w,T,t):
+    velocity = Velocity()
+    velocity.add_raw(u,v,w,T,t)
+    return velocity
+    
+def get_turbulence_wind(velocity,period):
+    temp = []
+    for i in [velocity.raw.u,velocity.raw.v,velocity.raw.w,velocity.raw.T,velocity.raw.t]:
+        temp.append(periodize(i,velocity.raw.t,period))
+    u = temp[0]
+    v = temp[1]
+    w = temp[2]
+    T = temp[3]
+    t = temp[4]
+    u,v,w,theta,phi = run_tilt_correction(u,v,w)
+    turbulence = Turbulence(u,v,w,T,t,period)
+    wind = run_wind_speed(u,v,w)
+    wind = Wind(wind,theta,phi,period)
+    velocity.add_turbulence(turbulence)
+    velocity.add_wind(wind)
+        
+def periodize(x,t,period):
     #period is in minutes, assuming t is in seconds
     Ts = t[1] - t[0]
-    L = period*60/Ts
-    t_mean = []
-    t_prime = []
+    L = int(round(period*60/Ts))
+    x_period = []
+    for i in range(t.size//L):
+        x_period.append(x[i*L:i*L+L+1])
+    x_period = x_period
+    return x_period
+
+def reynolds_decomposition(x):
+    x_mean = np.nanmean(x)
+    x_prime = x - x_mean
+    return x_mean, x_prime
+
+def run_reynolds_decomposition(x):
+    #input periodized u, v, w
     x_mean = []
     x_prime = []
-    for i in range(t.size//L):
-        x_mean.append(np.nanmean(x[i*L:i*L+L+1]))
-        x_prime.append(x[i*L:i*L+L+1]-x_mean[-1])
-        t_mean.append(np.nanmean(t[i*L:i*L+L+1]))
-        t_prime.append(t[i*L:i*L+L+1])
+    for i in range(len(x)):
+        temp_mean,temp_prime = reynolds_decomposition(x[i])
+        x_mean.append(temp_mean)
+        x_prime = np.append(x_prime,temp_prime)
     x_mean = np.array(x_mean)
-    x_prime = np.array(x_prime)
-    t_mean = np.array(t_mean)
-    t_prime = np.array(t_prime)
-    return x_mean, x_prime, t_mean, t_prime
+    return x_mean, x_prime
 
-L=floor(length(jd)/periodo);
-disp('Number of periods ...')
-disp(L);
+def wind_speed(u,v,w):
+    return (u**2 + v**2 + w**2)**.5
 
-#%----shrinking vectors to an integer number of periods for EC calculation
+def run_wind_speed(u,v,w):
+    #input periodized u, v, w
+    wind = []
+    for i in range(len(u)):
+        wind.append(wind_speed(np.nanmean(u[i]),np.nanmean(v[i]),np.nanmean(w[i])))
+    return wind
 
-u=u(1:L*periodo);
-v=v(1:L*periodo);
-w=w(1:L*periodo);
-T=T(1:L*periodo);
-jd=jd(1:L*periodo);
+def wind_direction(u,v):
+    return np.arctan2(v,u) #radians
 
-#%----wind direction 
+def wind_inclination(u,w):
+    return np.arctan2(w,u) #radians
 
-wdir = 180/pi*atan2(v,u);
+def tilt_correction(u,v,w):
+    u_mean,_ = reynolds_decomposition(u)
+    v_mean,_ = reynolds_decomposition(v)
+    w_mean,_ = reynolds_decomposition(w)
+    theta = wind_direction(u_mean,v_mean)
+    u = u * np.cos(theta) + v * np.sin(theta)
+    v = -u * np.sin(theta) + v * np.cos(theta)
+    u_mean,_ = reynolds_decomposition(u)
+    phi = wind_inclination(u_mean,w_mean)
+    u = u * np.cos(phi) + w * np.sin(phi)
+    w = -u * np.sin(phi) + w * np.cos(phi)
+    return u, v, w, theta, phi  
 
-#%-----reshaping the temporal series into matrices to start processing
-
-u1 = reshape(u,periodo,L);
-v1 = reshape(v,periodo,L);
-w1 = reshape(w,periodo,L);
-T1 = reshape(T,periodo,L);
-jd1= reshape(jd,periodo,L);
-wdir1= reshape(wdir,periodo,L);
-T3 = T1;
-T3 = mean(T3);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%-----------------------
-%----Correction for tilt in u,v,w-After this step w and v will have zero-mean
-%and the WS=sqrt(u^2+v^2) wil be equal to u
-u2=[];v2=[];w2=[];
-for k=1:L
-    [u2(:,k),v2(:,k),w2(:,k)]=tilt_correction(u1(:,k),v1(:,k),w1(:,k));
-end
-%----Detrend temperature
-%----Detrending the series
- for k=1:L,
-    %[p,s] = polyfit(jd1(:,k),u1(:,k),1);
-    %u1(:,k) = u1(:,k)-(p(1)*jd1(:,k)+p(2));
-    %[p,s] = polyfit(jd1(:,k),v1(:,k),1);
-    %v1(:,k) = v1(:,k)-(p(1)*jd1(:,k)+p(2));
-    %[p,s] = polyfit(jd1(:,k),w1(:,k),1);
-    %w1(:,k) = w1(:,k)-(p(1)*jd1(:,k)+p(2));
-    [p,s] = polyfit(jd1(:,k),T1(:,k),1);
-    T1(:,k) = T1(:,k)-(p(1)*jd1(:,k)+p(2));
- end
-
-%----Calculate Tprime & up by substracting means by blocks and wind speed module
-%TM = mean(T1);
-%UM = mean(u2);
-Tp = [];
-ws = [];
-for k=1:L
-    Tp(:,k) = T1(:,k) - mean(T1(:,k));
-    ws(:,k) = sqrt(u2(:,k).^2+v2(:,k).^2);
-    uc(:,k) = u2(:,k) - mean(u2(:,k));
-end
-%Outputs turbulent variables matrices to vectors
-jd = reshape(jd1,periodo*L,1);
-uc = reshape(uc,periodo*L,1); 
-vc = reshape(v2,periodo*L,1); 
-wc = reshape(w2,periodo*L,1); 
-Tc= reshape(Tp,periodo*L,1); 
-%output mean wind components
-ws = mean(ws); 
-wdir= mean(wdir1);
-jd1=mean(jd1);
-%----generating output structures
-turb.jd=jd;
-turb.up=uc;
-turb.vp=vc;
-turb.wp=wc;
-turb.tp=Tc;
-wind.jd = jd1;
-wind.ws = ws;
-wind.wd = wdir;
-wind.T = T3;
-%---------------------END------------------------------
-
-function [u2,v2,w2]=tilt_correction(u,v,w)
-%u,v,w are the three wind components LEV1 form the sonic anemometer at
-%one-height half hour data only
-%rotation around the horizontal axis into the mean wind direction
-vm = mean(v);
-um = mean(u);
-wm = mean(w);
-theta = atan(vm / um);
-%theta-Rotation 
-u1 = u * cos(theta) + v * sin(theta);
-v1 = -u * sin(theta) + v * cos(theta);
-w1 = w;
-%phi-Rotation
-%once the y-axis is compensated (mean =0) we rotates around it to reduce
-%the mean vertical wind component
-u1m = mean(u1);
-phi = atan(wm / u1m);
-u2 = u1 * cos(phi) + w1 * sin(phi);
-v2 = v1; 
-w2 = -u1 * sin(phi) + w1 * cos(phi);
+def run_tilt_correction(u,v,w):
+    #input periodized u, v, w
+    theta = np.zeros_like(u)
+    phi = np.zeros_like(theta)
+    for i in range(theta.size):
+        u[i], v[i], w[i], theta[i], phi[i] = tilt_correction(u[i],v[i],w[i])
+    return u, v, w, theta, phi
