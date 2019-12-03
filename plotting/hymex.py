a10efb2c19ade220f916317d324572d0420d1e7d
Douglas Keller <dkeller@loholt1.ipsl.polytechnique.fr>
2019-12-03 17:28:19 +0100

    updates

--- plotting/hymex.py
+++ plotting/hymex.py
@@ -1,8 +1,9 @@
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import PyKeller.data.model as PKdm
 
 def plotting_genoa_wrforch(Psl):
@@ -39,6 +40,8 @@ def plotting_genoa_winds_wrforch(uas,vas,Psl,time):
     plt.show()
     
 def plotting_mistral_wrforch(uas,vas,Psl,time):
+    ##########################################################################
+    # Setting up the data
     uas = uas.loc[time]
     vas = vas.loc[time]
     Psl = Psl.loc[time]
@@ -56,10 +59,14 @@ def plotting_mistral_wrforch(uas,vas,Psl,time):
     
     vec_scale = float(np.max(uas))*15
     
+    ##########################################################################
+    # Setting up the plot
+    
     plt.figure(figsize=(7,7))
     ax = plt.axes(projection=ccrs.Orthographic(8.9558,43.555));
+    europe_land_10m = cfeature.NaturalEarthFeature('physical','land','10m',edgecolor='black',facecolor='none')
+    ax.add_feature(europe_land_10m)    
     Psl.plot.contourf('nav_lon_grid_M','nav_lat_grid_M',ax=ax,transform=ccrs.PlateCarree(),cmap='rainbow',levels=30);
     plt.quiver(X,Y,uas_np,vas_np,transform=ccrs.PlateCarree(),scale=vec_scale,width=.0025);
-    ax.coastlines();
     ax.set_extent([1,9.5,40,45.5],crs=ccrs.PlateCarree());
     plt.show()
