a6319b37e86b1d6aed88e5639662495b2784bd7d
dkllrjr <dg.kllr.jr@gmail.com>
2020-03-19 08:35:51 +0100

    adding stuff done at home

--- data/nemo.py
+++ data/nemo.py
@@ -50,3 +50,42 @@ def find_loc(var,loc):
     ind = np.array(ind)
     
     return ind
+
+def find_loc_SI(SI,loc):
+    # loc needs to be a list of lists => [[lat,lon],[lat,lon]]
+    
+    lat = SI['latitude']
+    lon = SI['longitude']
+    date = SI['date']
+    
+    i = 0
+    ind_date = [0]
+    while i < len(date) - 1:
+        if date[i] < date[i+1]:
+            ind_date.append(i+1)
+        i += 1
+    ind_date.append(i+1)
+    
+    lat_lon = []
+    for i in range(len(lat)):
+        lat_lon.append([lat[i],lon[i]])
+        
+    ind_loc = []
+    for i in range(len(loc)):
+        ind_loc.append(PKdm.lat_lon_near_angle(lat_lon,loc[i]))
+        
+#    return ind_loc,ind_date
+
+    ind = []
+    
+    for i in range(len(ind_loc)):
+        ind.append([])
+        for j in range(len(ind_loc[i])):
+            for k in range(ind_date[j],ind_date[j+1]):
+#                print(tuple(lat_lon[k]),tuple(ind_loc[i][j]))
+                if tuple(lat_lon[k]) == tuple(ind_loc[i][j]):
+                    ind[i].append(k)
+                    
+        ind[i] = np.array(ind[i])
+    
+    return ind
--- data/sensitivity_analysis.py
+++ data/sensitivity_analysis.py
@@ -1,4 +1,6 @@
 import numpy as np
+import xarray as xr
+import pickle
 
 def conserve_direction(v,u):
     
@@ -30,3 +32,49 @@ def wind_field_filter(ug,vg,c,a):
 def wind_filter2(u,uth,a):
     
     return np.where(np.abs(u) < uth, u , np.sign(u)*(uth + a*(np.abs(u) - uth)))
+
+def stratification_index(N,z):
+    
+    return np.trapz(N*z,z)
+
+def nemo_stratification_index(N):
+    # N really equals N^2
+    d = N['depthw']
+    h_i = np.where(d<=1000)[0]
+    si = []
+    date = []
+    lat = []
+    lon = []
+    
+    for t in range(len(N['time_counter'])):
+        print(N['time_counter'][0][0:10],t)
+        for x in range(len(N['x_grid_W'])):
+            for y in range(len(N['y_grid_W'])):
+                temp_N = N.isel(time_counter=t,x_grid_W=x,y_grid_W=y)
+                temp_N = temp_N[h_i]
+                nan_i = np.where(temp_N!=np.nan)[0]
+                if nan_i.size != 0:
+                    temp_d = d[nan_i]
+                    date.append(str(temp_N['time_counter'].values))
+                    lat.append(np.float32(temp_N['nav_lat_grid_W'].values))
+                    lon.append(np.float32(temp_N['nav_lon_grid_W'].values))
+                    si.append(stratification_index(temp_N.values,temp_d.values))
+                    
+    SI = {'stratification_index':si,'latitude':lat,'longitude':lon,'date':date,'units':'m^2/s^2','description':'stratification index of the Gulf of Lion from a NEM0MED12 simulation'}
+    
+    return SI
+    
+def mp_N(rho_path,save_path):
+
+    rho = xr.open_dataset(rho_path) # vodensity, vovaisala
+    
+    print('Loaded ',rho_path)
+    
+    N = rho['vovaisala'].isel(y_grid_W=slice(151,210,1),x_grid_W=slice(165,208,1))
+        
+    SI = nemo_stratification_index(N)
+    
+    save_file_path = save_path + rho_path[-27:-6] + 'SI.pickle'
+    
+    with open(save_file_path,'wb') as file:
+        pickle.dump(SI,file)
